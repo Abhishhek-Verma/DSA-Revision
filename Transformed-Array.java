35869f084d8163d94fe1e3fe09627060af42a8d0
Abhishek Verma <abhi.ups838@gmail.com>
2026-02-05 17:23:20 +0530

    [3379] [Time Beats: 98.72%] [Memory Beats: 13.78%] - LeetPush

---
+++ Transformed-Array.java
@@ -0,0 +1,10 @@
+1class Solution {
+2    public int[] constructTransformedArray(int[] nums) {
+3        int n = nums.length;
+4        int[] res = new int[n];
+5        for(int i = 0; i < n; i ++){
+6            res[i] = nums[((i + nums[i]) % n + n) % n];
+7        }
+8        return res;
+9    }
+10}
