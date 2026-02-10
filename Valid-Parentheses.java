ab58ac5bdc945974194ac53af693277ee58aaf6d
Abhishek Verma <abhi.ups838@gmail.com>
2026-02-10 23:01:03 +0530

    [20] [Time Beats: 87.64%] [Memory Beats: 40.01%] - LeetPush

---
+++ Valid-Parentheses.java
@@ -0,0 +1,19 @@
+1class Solution {
+2    public boolean isValid(String s) {
+3        Stack<Character> stack = new Stack<>();
+4        for (char c : s.toCharArray()) {
+5            if (c == '(' || c == '{' || c == '[') {
+6                stack.push(c);
+7            } else {
+8                if (stack.isEmpty()) return false;
+9                char top = stack.pop();
+10                if ((c == ')' && top != '(') || 
+11                    (c == '}' && top != '{') || 
+12                    (c == ']' && top != '[')) {
+13                    return false;
+14                }
+15            }
+16        }
+17        return stack.isEmpty();
+18    }
+19}
