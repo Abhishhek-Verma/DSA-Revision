94aa1a1966b23a995507d77c45369ce78f99398b
Abhishek Verma <abhi.ups838@gmail.com>
2026-02-12 09:56:33 +0530

    [3713] [Time Beats: 28.46%] [Memory Beats: 79.40%] - LeetPush

--- Longest-Balanced-Substring-I.java
+++ Longest-Balanced-Substring-I.java
@@ -1,29 +1,21 @@
 1class Solution {
 2    public int longestBalanced(String s) {
 3        int n = s.length();
-4        int ans =0;
-5        for(int i =0;i<n;i++){
-6            int[] cnt = new int[26]; 
-7            for(int j = i;j<n;j++){
-8                cnt[s.charAt(j)-'a']++;
-9                int freq = 0;
-10                boolean t = false;
-11                for(int x: cnt){
-12                    if(x>0){
-13                        if(freq==0){
-14                            freq=x;
-15                        }
-16                        else if(freq!=x){
-17                            t = true;
-18                            break;
-19                        }
-20                    }
-21                }
-22                if(!t){
-23                    ans = Math.max(ans,j-i+1);
-24                }
-25            }
-26        }
-27        return ans;
-28    }
-29}
+4        int ans = 0;
+5        for (int i = 0; i < n; i++) {
+6            HashMap<Character, Integer> mpp = new HashMap<>();
+7            int max = 0;
+8            for (int j = i; j < n; j++) {
+9                char c = s.charAt(j);
+10                mpp.put(c, mpp.getOrDefault(c, 0) + 1);
+11                max = Math.max(max, mpp.get(c));
+12                int len = j - i + 1;
+13                int dst = mpp.size();
+14                if (max * dst == len) {
+15                    ans = Math.max(ans, len);
+16                }
+17            }
+18        }
+19        return ans;
+20    }
+21}
