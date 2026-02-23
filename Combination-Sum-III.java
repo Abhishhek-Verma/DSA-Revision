bb6fb855bc7bffeb6f3c804e6b4aa4d844531278
Abhishek Verma <abhi.ups838@gmail.com>
2026-02-23 16:50:54 +0530

    [216] [Time Beats: 100.00%] [Memory Beats: 12.11%] - LeetPush

--- Combination-Sum-III.java
+++ Combination-Sum-III.java
@@ -1,20 +1,19 @@
 1class Solution {
-2    private static List<List<Integer>> lst = new ArrayList<>();
+2    private List<List<Integer>> lst = new ArrayList<>();
 3    public List<List<Integer>> combinationSum3(int k, int n) {
-4        lst.clear();
-5        check(k, n, new ArrayList<>(), 0, 1);
-6        return lst;
-7    }
-8    private static void check(int k, int n, List<Integer> ds, int sum, int num){
-9        if(k == 0 && sum == n){
-10            lst.add(new ArrayList<>(ds));
-11            return;
-12        }
-13        if(k == 0 || sum > n || num > 9) return;
-14
-15        ds.add(num);
-16        check(k-1, n, ds, sum+num, num+1);
-17        ds.remove(ds.size()-1);
-18        check(k, n, ds, sum, num+1);
-19    }
-20}
+4        check(k, n, new ArrayList<>(), 0, 1);
+5        return lst;
+6    }
+7    private  void check(int k, int n, List<Integer> ds, int sum, int num){
+8        if(k == 0 && sum == n){
+9            lst.add(new ArrayList<>(ds));
+10            return;
+11        }
+12        if(k == 0 || sum > n || num > 9) return;
+13
+14        ds.add(num);
+15        check(k-1, n, ds, sum+num, num+1);
+16        ds.remove(ds.size()-1);
+17        check(k, n, ds, sum, num+1);
+18    }
+19}
