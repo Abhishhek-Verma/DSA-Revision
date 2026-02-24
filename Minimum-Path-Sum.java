089aa6638ffdd313ecdfe62a82c3a80d1d7865b4
Abhishek Verma <abhi.ups838@gmail.com>
2026-02-24 16:14:24 +0530

    [64] [Time Beats: 80.84%] [Memory Beats: 95.30%] - LeetPush

--- Minimum-Path-Sum.java
+++ Minimum-Path-Sum.java
@@ -3,24 +3,23 @@
 3    public int minPathSum(int[][] mm) {
 4        int m = mm.length;
 5        int n = mm[0].length;
-6        // int[][] vis = new int[m][n];
-7        int[] di = {1, 0};
-8        int[] dj = {0, 1};
-9        Integer[][] dp = new Integer[m][n];
-10        return solve(0, 0, mm, di, dj, m, n, 0, dp);
-11    }
-12
-13private int solve(int r, int c, int[][] a, int[] di, int[] dj, int m, int n, int curr, Integer[][] dp) {
-14        if (r == m - 1 && c == n - 1) return a[r][c];
-15        if (dp[r][c] != null) return dp[r][c];
-16        int best = Integer.MAX_VALUE;
-17        for (int i = 0; i < 2; i++) {
-18            int nextR = r + di[i];
-19            int nextC = c + dj[i];
-20            if (nextR < m &&  nextC < n ) {
-21                best = Math.min(best, solve(nextR, nextC, a, di, dj, m, n, curr + a[r][c], dp));
-22            }
-23        }
-24        return dp[r][c] = a[r][c] + (best == Integer.MAX_VALUE ? 0 : best);
-25    }
-26}
+6        int[] di = {1, 0};
+7        int[] dj = {0, 1};
+8        Integer[][] dp = new Integer[m][n];
+9        return solve(0, 0, mm, di, dj, m, n, 0, dp);
+10    }
+11
+12private int solve(int r, int c, int[][] a, int[] di, int[] dj, int m, int n, int curr, Integer[][] dp) {
+13        if (r == m - 1 && c == n - 1) return a[r][c];
+14        if (dp[r][c] != null) return dp[r][c];
+15        int best = Integer.MAX_VALUE;
+16        for (int i = 0; i < 2; i++) {
+17            int nextR = r + di[i];
+18            int nextC = c + dj[i];
+19            if (nextR < m &&  nextC < n ) {
+20                best = Math.min(best, solve(nextR, nextC, a, di, dj, m, n, curr + a[r][c], dp));
+21            }
+22        }
+23        return dp[r][c] = a[r][c] + (best == Integer.MAX_VALUE ? 0 : best);
+24    }
+25}
