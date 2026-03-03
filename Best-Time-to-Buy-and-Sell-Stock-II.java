5c48722ccf99abb3e93e10ea56eb6fbd3bb595ff
Abhishek Verma <abhi.ups838@gmail.com>
2026-03-03 08:12:22 +0530

    [122] [Time Beats: 3.20%] [Memory Beats: 5.32%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-II.java
+++ Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -1,11 +1,19 @@
 1class Solution {
-2    public int maxProfit(int[] nums) {
-3        int ans =0;
-4        for(int i =1;i<nums.length;i++){
-5            if(nums[i]>nums[i-1]){
-6                ans+=nums[i]- nums[i-1];
-7            }
-8        }
-9        return ans;
-10    }
-11}
+2    Integer[][]dp;
+3    public int maxProfit(int[] nums) {
+4        dp= new Integer[nums.length][2];
+5        int k = 1;
+6      return  solve(nums,0,nums.length,k);
+7    }
+8    private int solve(int[]nums, int idx, int n , int k){
+9        if(idx==n) return 0;
+10        if(dp[idx][k]!=null) return dp[idx][k];
+11        int profit;
+12        if(k==1){
+13          profit = Math.max(-nums[idx]+ solve(nums,idx+1,n,0),0+solve(nums,idx+1,n,1));
+14        }else{
+15            profit = Math.max(nums[idx]+solve(nums,idx+1,n,1),0+solve(nums, idx+1,n , 0));
+16        }
+17        return dp[idx][k]=profit;
+18    }
+19}
