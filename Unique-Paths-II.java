4a315d4bacdffc2dec78d9d3f4e75fe3d266f1fb
Abhishek Verma <abhi.ups838@gmail.com>
2026-03-04 09:49:00 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 64.50%] - LeetPush

--- Unique-Paths-II.java
+++ Unique-Paths-II.java
@@ -1,18 +1,22 @@
 1class Solution {
 2    public int uniquePathsWithObstacles(int[][] mm) {
-3      int m = mm.length;
+3        int m = mm.length;
 4        int n = mm[0].length;
-5        if(mm[0][0]==1) return 0;
-6        Integer[][] dp = new Integer[m][n];
-7        return solve(0,0,mm,m,n,dp);
-8    }
-9
-10    private int solve(int r,int c,int[][] a,int m,int n,Integer[][] dp){
-11        if(r>=m || c>=n || a[r][c]==1) return 0;
-12        if(r==m-1 && c==n-1) return 1;
-13        if(dp[r][c]!=null) return dp[r][c];
-14        int down = solve(r+1,c,a,m,n,dp);
-15        int right = solve(r,c+1,a,m,n,dp);
-16        return dp[r][c]=down+right;
-17}
-18}
+5       int[][]dp = new int[m][n];
+6        for(int i =0;i<m;i++){
+7            for(int j =0;j<n;j++){
+8                if(mm[i][j]==1) dp[i][j]= 0;
+9                else if(i==0 && j==0){ 
+10                    dp[i][j]=1;
+11                }
+12                else{
+13                    int up=0,down=0;
+14                   if(i>0) up =dp[i-1][j];
+15                    if(j>0) down =dp[i][j-1];
+16                dp[i][j]= up+down;
+17                }
+18            }
+19        }
+20    return dp[m-1][n-1];
+21}
+22}
