b112f3e4f665db79687c3388f3ad92a424fd1fd5
Abhishek Verma <abhi.ups838@gmail.com>
2026-03-04 06:56:38 +0530

    [115] [Time Beats: 69.63%] [Memory Beats: 36.94%] - LeetPush

--- Distinct-Subsequences.java
+++ Distinct-Subsequences.java
@@ -1,17 +1,20 @@
 1class Solution {
-2    Integer[][]dp;
-3    public int numDistinct(String s, String t) {
-4        int n = s.length();
-5        int m = t.length();
-6        dp = new Integer [n][m];
-7        return solve(n-1,m-1,s,t);
-8    }
-9    private int solve(int i, int j, String s , String t){
-10        if(j<0) return 1;
-11        if(i<0) return 0;
-12        if(dp[i][j]!=null) return dp[i][j];
-13        if(s.charAt(i)==t.charAt(j))
-14            return dp[i][j]= solve(i-1,j-1,s,t) + solve(i-1,j,s,t);
-15        return  dp[i][j]  = solve(i-1,j,s,t);
-16    }
-17}
+2    public int numDistinct(String s, String t) {
+3        int n = s.length();
+4        int m = t.length();
+5        int[][]dp = new int [n+1][m+1];
+6        for(int i=0;i<=n;i++) dp[i][m] = 1;
+7
+8        for(int i =n-1;i>=0;i--){
+9            for(int j = m-1;j>=0;j--){
+10                if(s.charAt(i)==t.charAt(j)){
+11                    dp[i][j]= dp[i+1][j+1]+ dp[i+1][j];
+12                }
+13                else{
+14                    dp[i][j]= dp[i+1][j];
+15                }
+16            }
+17        }
+18        return dp[0][0];
+19    }
+20}
