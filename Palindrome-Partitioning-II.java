2d9fcf087e2da9e27d624e4654f74fcc57868d58
Abhishek Verma <abhi.ups838@gmail.com>
2026-03-18 21:12:55 +0530

    [132] [Time Beats: 47.04%] [Memory Beats: 52.50%] - LeetPush

--- Palindrome-Partitioning-II.java
+++ Palindrome-Partitioning-II.java
@@ -1,31 +1,34 @@
 1class Solution {
-2  static int[] dp;
-3  public int minCut(String s) {
-4    int n = s.length();
-5    dp = new int[n];
-6    Arrays.fill(dp, -1);
-7    return solve(0, s) - 1;
-8    }
-9   private int solve(int idx, String s) {
-10    if (idx == s.length()) return 0;
-11    if (dp[idx] != -1) return dp[idx];
-12
-13    int min = Integer.MAX_VALUE;
-14    for (int i = idx; i < s.length(); i++) {
-15        if (isPalindrome(idx, i, s)) {
-16            int cuts = 1 + solve(i + 1, s);
-17            min = Math.min(min, cuts);
+2    static int[] dp;
+3    static boolean[][] pal;
+4
+5    public int minCut(String s) {
+6        int n = s.length();
+7        dp = new int[n];
+8        Arrays.fill(dp, -1);
+9        pal = new boolean[n][n];
+10        // Precompute palindrome table
+11        for (int i = n - 1; i >= 0; i--) {
+12            for (int j = i; j < n; j++) {
+13                if (s.charAt(i) == s.charAt(j)) {
+14                    if (j - i <= 2) pal[i][j] = true;
+15                    else pal[i][j] = pal[i + 1][j - 1];
+16                }
+17            }
 18        }
-19    }
-20
-21    return dp[idx] = min;
-22}
-23    private static boolean isPalindrome(int i, int j, String s) {
-24        while (i <= j) {
-25            if (s.charAt(i++) != s.charAt(j--)) {
-26                return false;
-27            }
-28        }
-29        return true;
-30    }
-31}
+19        return solve(0, s) - 1;
+20    }
+21    private int solve(int idx, String s) {
+22        if (idx == s.length()) return 0;
+23        if (dp[idx] != -1) return dp[idx];
+24
+25        int min = Integer.MAX_VALUE;
+26        for (int i = idx; i < s.length(); i++) {
+27            if (pal[idx][i]) {   // O(1) check
+28                int cuts = 1 + solve(i + 1, s);
+29                min = Math.min(min, cuts);
+30            }
+31        }
+32        return dp[idx] = min;
+33    }
+34}
