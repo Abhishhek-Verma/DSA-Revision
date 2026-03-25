8f050b52ae9910534752a932d9e71082c18addf4
Abhishek Verma <abhi.ups838@gmail.com>
2026-03-25 12:28:30 +0530

    [1671] [Time Beats: 63.89%] [Memory Beats: 75.74%] - LeetPush

--- Minimum-Number-of-Removals-to-Make-Mountain-Array.java
+++ Minimum-Number-of-Removals-to-Make-Mountain-Array.java
@@ -1,32 +1,36 @@
-1class Solution {
-2    public int minimumMountainRemovals(int[] nums) {
-3         int n = nums.length;
-4        int[] lis = new int[n];
-5        int[] lds = new int[n];
-6        // LIS from left
-7        for (int i = 0; i < n; i++) {
-8            lis[i] = 1;
-9            for (int j = 0; j < i; j++) {
-10                if (nums[j] < nums[i]) {
-11                    lis[i] = Math.max(lis[i], lis[j] + 1);
-12                }
-13            }
-14        }
-15        // LDS from right
-16        for (int i = n - 1; i >= 0; i--) {
-17            lds[i] = 1;
-18            for (int j = n - 1; j > i; j--) {
-19                if (nums[j] < nums[i]) {
-20                    lds[i] = Math.max(lds[i], lds[j] + 1);
-21                }
-22            }
-23        }
-24        int maxMountain = 0;
-25        for (int i = 0; i < n; i++) {
-26            if (lis[i] > 1 && lds[i] > 1) {
-27                maxMountain = Math.max(maxMountain, lis[i] + lds[i] - 1);
-28            }
-29        }
-30        return n - maxMountain; 
-31    }
-32}
+1//main intuition ye h ki mai jis index pe hu 
+2//us index pr usse phle kitne inc and kitne dec element hai
+3//then try to maximize it,tabhi minimum removal of elements hoga 
+4
+5class Solution {
+6    public int minimumMountainRemovals(int[] nums) {
+7         int n = nums.length;
+8        int[] lis = new int[n];
+9        int[] lds = new int[n];
+10        // LIS from left
+11        for (int i = 0; i < n; i++) {
+12            lis[i] = 1;
+13            for (int j = 0; j < i; j++) {
+14                if (nums[j] < nums[i]) {
+15                    lis[i] = Math.max(lis[i], lis[j] + 1);
+16                }
+17            }
+18        }
+19        // LDS from right
+20        for (int i = n - 1; i >= 0; i--) {
+21            lds[i] = 1;
+22            for (int j = n - 1; j > i; j--) {
+23                if (nums[j] < nums[i]) {
+24                    lds[i] = Math.max(lds[i], lds[j] + 1);
+25                }
+26            }
+27        }
+28        int maxMountain = 0;
+29        for (int i = 0; i < n; i++) {
+30            if (lis[i] > 1 && lds[i] > 1) {
+31                maxMountain = Math.max(maxMountain, lis[i] + lds[i] - 1);
+32            }
+33        }
+34        return n - maxMountain; 
+35    }
+36}
