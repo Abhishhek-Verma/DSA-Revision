6eee29d557fc4da43b74270636969f689ee8848e
Abhishek Verma <abhi.ups838@gmail.com>
2026-08-16 09:01:55 +0530

    [583] [Time Beats: 95.02%] [Memory Beats: 32.43%] - LeetPush

---
+++ 583-Delete-Operation-for-Two-Strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minDistance(String s, String t) {
+           return s.length()+t.length()-2*lcs(s,t);
+    }
+    private int lcs(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (s.charAt(i) == t.charAt(j))
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+}
