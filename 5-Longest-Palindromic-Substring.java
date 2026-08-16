54b368d85ce9b3e5bd6166ee6ccbbf77d6d7c4a4
Abhishek Verma <abhi.ups838@gmail.com>
2026-08-16 11:50:34 +0530

    [5] [Time Beats: 19.35%] [Memory Beats: 5.32%] - LeetPush

---
+++ 5-Longest-Palindromic-Substring.java
@@ -0,0 +1,29 @@
+class Solution {
+    Boolean[][] dp;
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        dp = new Boolean[n][n];
+        int start = 0;
+        int maxLen = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(s, i, j)) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+    private boolean isPalindrome(String s, int i, int j) {
+        if (i >= j)  return true;
+        if (dp[i][j] != null)
+            return dp[i][j];
+        if (s.charAt(i) != s.charAt(j))
+            return dp[i][j] = false;
+
+        return dp[i][j] = isPalindrome(s, i + 1, j - 1);
+    }
+}
